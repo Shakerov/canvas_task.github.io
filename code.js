d1dd5d5bd8e7271d41cc22303714c87f8c456660
Shakerov <42868699+Shakerov@users.noreply.github.com>
2021-03-28 23:15:56 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,5 @@
 let canvas = document.querySelector("#canvas");
 let canvas2 = document.querySelector("#canvas2");
-//let picture = document.querySelector("#img");
 let ctx = canvas.getContext("2d");
 let ctx2 = canvas2.getContext("2d");
 let img = new Image();
@@ -14,14 +13,30 @@ img2.src="cat_face_eyes_gray_striped_green-794646.jpeg";
 img2.onload = function(){
   ctx2.drawImage(img2, 0, 0);
   let pixels = ctx2.getImageData(0,0, img2.width, img2.height);
-  let t = 3;
-  for(let i=0; i<pixels.data.length; i++){
-    if(i == t){
-      t = i + 4;
-      continue;
-    }
-    pixels.data[i] = pixels.data[i] * 1.7;
+  let r = pixels.data[0];
+  let g = pixels.data[1];
+  let b = pixels.data[2];
+  const brightConst = 1.7;
+  for(let i=0; i<pixels.data.length; i+=4){
+    
+    r *= brightConst;
+    g *= brightConst;
+    b *= brightConst;
   }
   ctx2.putImageData(pixels, 0, 0);
 }
 
+// Another way
+// img2.onload = function(){
+//   ctx2.drawImage(img2, 0, 0);
+//   let pixels = ctx2.getImageData(0,0, img2.width, img2.height);
+//   let t = 3;
+//   for(let i=0; i<pixels.data.length; i++){
+//     if(i == t){
+//       t = i + 4;
+//       continue;
+//     }
+//     pixels.data[i] = pixels.data[i] * 1.7;
+//   }
+//   ctx2.putImageData(pixels, 0, 0);
+// }
